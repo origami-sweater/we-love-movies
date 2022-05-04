6ba9637948ae1586eaa12c2e36f5cfdbd5eee6f7
origami-sweater <kelsey.fraleigh@gmail.com>
2022-05-03 22:27:31 -0500

    fixed theaters issue, added methodNotAllowed to routers

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const controller = require("./reviews.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
     .route("/:reviewId")
     .get(controller.readReview)
     .put(controller.update)
     .delete(controller.delete)
+    .all(methodNotAllowed);
 
 router
     .route("/")
+    .all(methodNotAllowed);
 
 module.exports = router;
